a5f9130c8653abf1914d1818e21e6accf4dd6d16
jtellis1991 <joshuaellis1991@gmail.com>
2021-04-07 20:27:59 +0000

    more more done with dem koans

--- triangle.rb
+++ triangle.rb
@@ -14,7 +14,19 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if a == 0 || b == 0 || c == 0
+    raise TriangleError, "Sides of Trainge cannot be zero"
+  elsif a < 0 || b < 0 || c < 0
+    raise TriangleError, "Sides of Triangel cannot be negative"
+  elsif (a + b <= c) || (a + c <= b) || (b + c <= b)
+    raise TriangleError, "Violation of Triangle Inequality Theorem"
+  elsif a == b && a == c
+    :equilateral
+  elsif a == b || a == c || b == c 
+    :isosceles
+  else
+    :scalene
+  end
 end
 
 # Error class used in part 2.  No need to change this code.
